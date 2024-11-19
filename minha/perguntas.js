0189f379867a513d0ddb5d53e41e2f3e812fb881
Bryan <132940398+bryangasbriel@users.noreply.github.com>
2024-11-19 10:04:01 -0300

    Add files via upload

---
+++ minha/perguntas.js
@@ -0,0 +1,19 @@
+criaCartao(
+'programaçao',
+'oque e javaScript',
+'o javaScript é uma linguagemde programaçao'
+)
+
+criaCartao(
+    'programaçao',
+    'quem perdeu pro corinthians ontem',
+    'palmeiras'
+    )
+
+    criaCartao(
+        'programaçao',
+        'qual a capital da russia',
+        'moscou'
+        )
+
+       
